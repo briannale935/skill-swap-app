884a2e18cc610ab5641653190a783caa707c5f33
surayamanji <144382352+surayamanji@users.noreply.github.com>
2025-01-17 09:51:06 -0500

    Update ReviewBody.js
    Edited label text

--- client/src/components/App/ReviewBody.js
+++ client/src/components/App/ReviewBody.js
@@ -4,8 +4,8 @@ import { TextField } from '@mui/material';
 function ReviewBody({ enteredReview, handleReviewChange }) {
   return (
     <TextField
-      id="review-body"
-      label="Enter your review"
+      id="review-body!"
+      label="Enter your review!"
       value={enteredReview}
       onChange={handleReviewChange}
       multiline
