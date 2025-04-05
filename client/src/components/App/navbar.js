51e33f0b56296845bd83a0af310af382ef886b64
Aithy Ngoc Nguyen <125173652+aithynnguyen@users.noreply.github.com>
2025-04-05 20:51:24 +0000

    working navbar

--- client/src/components/App/navbar.js
+++ client/src/components/App/navbar.js
@@ -1,45 +1,26 @@
-import { AppBar, Toolbar, Button, Typography, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#52ab98', boxShadow: 3 }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        {/* Left side buttons */}
-        <Box sx={{ display: 'flex', gap: 2 }}>
-          <Button
-            component={Link}
-            to="/Search"
-            sx={{ color: 'white', fontWeight: 'bold', '&:hover': { backgroundColor: '#3e8e7e' } }}
-          >
-            Search
-          </Button>
-          <Button
-            component={Link}
-            to="/Matches"
-            sx={{ color: 'white', fontWeight: 'bold', '&:hover': { backgroundColor: '#3e8e7e' } }}
-          >
-            Matches
-          </Button>
-          <Button
-            component={Link}
-            to="/PostCreation"
-            sx={{ color: 'white', fontWeight: 'bold', '&:hover': { backgroundColor: '#3e8e7e' } }}
-          >
-            Blog
-          </Button>
-        </Box>
-
-        {/* Right side Profile button */}
-        <Box>
-          <Button
-            component={Link}
-            to="/Profile"
-            sx={{ color: 'white', fontWeight: 'bold', '&:hover': { backgroundColor: '#3e8e7e' } }}
-          >
-            Profile
-          </Button>
-        </Box>
+    <AppBar position="static" sx={{ bgcolor: '#52ab98' }}>
+      <Toolbar>
+        <Button color="inherit" component={Link} to="/Search">
+          Search
+        </Button>
+        <Button color="inherit" component={Link} to="/Matches">
+          Matches
+        </Button>
+        <Button color="inherit" component={Link} to="/PostCreation">
+          Blog
+        </Button>
+        <Button color="inherit" component={Link} to="/MyReviews">
+          My Reviews
+        </Button>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button color="inherit" component={Link} to="/Profile">
+          Profile
+        </Button>
       </Toolbar>
     </AppBar>
   );
